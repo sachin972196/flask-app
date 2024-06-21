6f084aac7e9d829222636820154a6c40d72c8545
sachin972196 <147017244+sachin972196@users.noreply.github.com>
2024-06-21 23:10:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-# app.py
-
 from flask import Flask, render_template, request
 from script import get_instagram_video_url  # Import the function from script.py
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DEBUG'] = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't', 'y', 'yes']
 
 @app.route('/')
 def index():
@@ -19,4 +23,4 @@ def generate():
         return render_template('index.html', video_url=None, error=video_url_or_error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()

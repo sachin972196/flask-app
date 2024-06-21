47f0f0664052f6aceb7b3635a55f50fb0c466e7a
sachin972196 <147017244+sachin972196@users.noreply.github.com>
2024-06-22 00:44:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,3 @@
-# script.py
-
 import instaloader
 from instaloader.exceptions import InstaloaderException
 
@@ -7,10 +5,12 @@ def get_instagram_video_url(url):
     try:
         loader = instaloader.Instaloader()
         post = instaloader.Post.from_shortcode(loader.context, url.rsplit('/', 2)[1])
+        
         if post.typename == "GraphVideo":
             video_url = post.video_url
             return True, video_url
         else:
             return False, "No video found at the provided URL."
+    
     except InstaloaderException as e:
         return False, f"Error fetching video URL: {e}"

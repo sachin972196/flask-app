4f17474c6c12c32924f340e515fdd91daec464c5
sachin972196 <147017244+sachin972196@users.noreply.github.com>
2024-06-22 01:02:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,27 +1,16 @@
- 
+# script.py
 
 import instaloader
-from instaloader.exceptions import InstaloaderException, QueryReturnedNotFoundException
+from instaloader.exceptions import InstaloaderException
 
 def get_instagram_video_url(url):
     try:
         loader = instaloader.Instaloader()
-
-        # Assuming you've completed the verification process manually
-        # Replace 'your_username' and 'your_password' with your actual Instagram credentials
-        loader.login('python_code121', 'sachin@sach123')
-
         post = instaloader.Post.from_shortcode(loader.context, url.rsplit('/', 2)[1])
-        
         if post.typename == "GraphVideo":
             video_url = post.video_url
             return True, video_url
         else:
             return False, "No video found at the provided URL."
-    
-    except QueryReturnedNotFoundException:
-        return False, "Instagram post not found."
-    
     except InstaloaderException as e:
         return False, f"Error fetching video URL: {e}"
-

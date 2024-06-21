9b4ab680d0c1966ef824d6f5390801f14c591773
sachin972196 <147017244+sachin972196@users.noreply.github.com>
2024-06-22 00:44:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import logging
 from flask import Flask, render_template, request
 from script import get_instagram_video_url
 
 app = Flask(__name__)
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -18,7 +14,6 @@ def generate():
     if success:
         return render_template('index.html', video_url=video_url_or_error, error=None)
     else:
-        logging.error(f"Error fetching video URL: {video_url_or_error}")
         return render_template('index.html', video_url=None, error=video_url_or_error)
 
 if __name__ == '__main__':
